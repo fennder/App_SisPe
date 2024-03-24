5e6c5d8e1c0970b3966b3e2f436e9d323cfbd859
Edir Pereira (Brasília-DF) <fennder@gmail.com>
2024-03-24 14:28:35 -0300

    First Commit for App SisPe

---
+++ src/main/java/br/com/abrindoportas/SisPe/SisPeApplication.java
@@ -0,0 +1,13 @@
+package br.com.abrindoportas.SisPe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisPeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisPeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/abrindoportas/SisPe/SisPeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abrindoportas.SisPe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisPeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
